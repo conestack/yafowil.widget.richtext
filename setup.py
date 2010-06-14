95ec4ea7d0684efd063b4350746d5d451ef4f109
rnix <rnix@f119a143-da5b-4457-81e3-bee00661a780>
2010-06-14 11:50:32 +0000

    git-svn-id: https://svn.bluedynamics.eu/svn/module/yafowil.widget.richtext/trunk@2538 f119a143-da5b-4457-81e3-bee00661a780

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0'
+shortdesc = 'yafowil.widget.richtext'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+setup(name='yafowil.widget.richtext',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python', 
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',        
+      ],
+      keywords='',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      url=u'https://svn.bluedynamics.eu/svn/module/yafowil.widget.richtext',
+      license='GNU General Public Licence',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['yafowil', 'yafowil.widget'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'yafowil',
+      ],
+      extras_require = dict(),
+      tests_require=[
+          'interlude',
+      ],
+      test_suite="yafowil.widget.richtext.tests.test_suite",
+      entry_points = """\
+      """        
+      )
+
