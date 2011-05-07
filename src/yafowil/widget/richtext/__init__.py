2bf77f022d31d27c10425e913ffe0bb8476772c0
Robert Niederreiter <rnix@squarewave.at>
2011-05-07 17:06:48 +0200

    formatting, test coverage

--- src/yafowil/widget/richtext/__init__.py
+++ src/yafowil/widget/richtext/__init__.py
@@ -2,5 +2,4 @@ import widget
 try:
     from pyramid.view import static
     tiny_mce = static('tinymce/jscripts/tiny_mce')
-except ImportError:
-    pass
+except ImportError: pass                                    #pragma NO COVERAGE
