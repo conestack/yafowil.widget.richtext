21efac2ea1d4c838c354b8cea156436ace9464db
Robert Niederreiter <rnix@squarewave.at>
2012-02-29 14:25:27 +0100

    tmp comment

--- src/yafowil/widget/richtext/__init__.py
+++ src/yafowil/widget/richtext/__init__.py
@@ -3,29 +3,30 @@ import os
 def register():
     import widget
     
-def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+def get_resource_dir(*additional):
+    return os.path.join(os.path.dirname(__file__), 'resources', *additional)
 
 def _walk_third_parties(path,postfix):
     results = []
-    path_base_len = len(get_resource_dir)
+    path_base_len = len(get_resource_dir())
     def visitor(arg, dirname, names):
         for name in names:
             if name.endswith(postfix):
-                filepath = os.path.join(dirname,name)
-                results.append(filepath[:path_base_len.strip(os.sep)])
+                filepath = os.path.join(dirname, name).strip(os.sep)
+                results.append(filepath[:path_base_len])
     os.path.walk(path, visitor,None)
     return results
         
 def get_js(thirdparty=True):
     js = ['widget.js']
-    if thirdparty:
-        target_path = os.path.join(get_resource_dir('tinymce'))
-        js.append(_walk_third_parties(target_path,'.js'))
+    #if thirdparty:
+    #    target_path = os.path.join(get_resource_dir('tinymce'))
+    #    js.append(_walk_third_parties(target_path,'.js'))
     return js
 
 def get_css(thirdparty=True):
-    if not thirdparty:
-        return []
-    target_path = os.path.join(get_resource_dir('tinymce'))
-    return _walk_third_parties(target_path,'.css')
+    #if not thirdparty:
+    #    return []
+    #target_path = os.path.join(get_resource_dir('tinymce'))
+    #return _walk_third_parties(target_path,'.css')
+    return []
