986077714bb40e3fc44afd8053ccc3e0cb41fe94
Jens W. Klein <jens@bluedynamics.com>
2012-06-13 00:24:24 +0200

    refactored examples

--- src/yafowil/widget/richtext/example.py
+++ src/yafowil/widget/richtext/example.py
@@ -1,9 +1,8 @@
 from yafowil.base import factory
 
-
 def get_example():
     part = factory(u'fieldset', name='yafowilwidgetrichtext')
     part['richtext'] = factory('field:label:error:richtext', props={
         'label': 'TinyMCE richtext field',
         'required': 'Text is required'})
-    return {'widget': part, 'routes': {}}
+    return [{'widget': part, 'doc': 'TODO'}]
