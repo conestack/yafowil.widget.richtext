ce14aab298da1160b93fc820d1c00cb647a02150
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:23:11 +0200

    resources

--- src/yafowil/widget/richtext/__init__.py
+++ src/yafowil/widget/richtext/__init__.py
@@ -1,25 +1,37 @@
 import os 
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'tinymce/jscripts/tiny_mce/tiny_mce.js',
+    'thirdparty': True,
+    'order': 20,
+}, {
+    'resource': 'tinymce/jscripts/tiny_mce/jquery.tinymce.js',
+    'thirdparty': False,
+    'order': 21,
+}, {
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 22,
+}]
 
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.richtext',
+                           resourcedir, js=js)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [{
-        'resource': 'tinymce/jscripts/tiny_mce/tiny_mce.js',
-        'thirdparty': True,
-        'order': 20,
-    }, {
-        'resource': 'tinymce/jscripts/tiny_mce/jquery.tinymce.js',
-        'thirdparty': False,
-        'order': 21,
-    }, {
-        'resource': 'widget.js',
-        'thirdparty': False,
-        'order': 22,
-    }]
+    return js
