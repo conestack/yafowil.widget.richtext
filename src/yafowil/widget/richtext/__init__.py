c3084001249f0491f2d48aa9662b4dae16f48daa
Robert Niederreiter <office@squarewave.at>
2012-07-18 11:57:43 +0200

    resources

--- src/yafowil/widget/richtext/__init__.py
+++ src/yafowil/widget/richtext/__init__.py
@@ -2,19 +2,20 @@ import os
 from yafowil.base import factory
 
 
+# XXX: use fanstatic
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 
 js = [{
+    'group': 'richtext',
     'resource': 'tinymce/jscripts/tiny_mce/tiny_mce.js',
-    'thirdparty': True,
     'order': 20,
 }, {
+    'group': 'richtext',
     'resource': 'tinymce/jscripts/tiny_mce/jquery.tinymce.js',
-    'thirdparty': False,
     'order': 21,
 }, {
+    'group': 'yafowil.widget.richtext',
     'resource': 'widget.js',
-    'thirdparty': False,
     'order': 22,
 }]
 
