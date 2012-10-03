d0f257304ee763bd30b6fd0ee5d3f8e70dc860cf
Robert Niederreiter <office@squarewave.at>
2012-10-03 10:49:15 +0200

    resource group names

--- src/yafowil/widget/richtext/__init__.py
+++ src/yafowil/widget/richtext/__init__.py
@@ -4,15 +4,15 @@ from yafowil.base import factory
 
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 js = [{
-    'group': 'richtext',
+    'group': 'yafowil.widget.richtext.dependencies',
     'resource': 'tinymce/jscripts/tiny_mce/tiny_mce.js',
     'order': 20,
 }, {
-    'group': 'richtext',
+    'group': 'yafowil.widget.richtext.dependencies',
     'resource': 'tinymce/jscripts/tiny_mce/jquery.tinymce.js',
     'order': 21,
 }, {
-    'group': 'yafowil.widget.richtext',
+    'group': 'yafowil.widget.richtext.common',
     'resource': 'widget.js',
     'order': 22,
 }]
