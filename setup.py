5dc955f7501dc98bf5b18d3c0ffc5cd8a85eb86b
Robert Niederreiter <office@squarewave.at>
2015-01-23 17:01:30 +0100

    prepare 1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (
 )
 
 
-version = '1.3.1'
+version = '1.3.2'
 shortdesc = 'Richtext Widget for YAFOWIL'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
