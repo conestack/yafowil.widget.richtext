59a8fc8a57eee342eb257d5c67d1eb1cbf9dea08
Robert Niederreiter <office@squarewave.at>
2018-06-15 09:51:12 +0200

    Add mimetype selection support to richtext widget

--- src/yafowil/widget/richtext/widget.py
+++ src/yafowil/widget/richtext/widget.py
@@ -3,6 +3,9 @@ from yafowil.base import fetch_value
 from yafowil.common import generic_extractor
 from yafowil.common import generic_required_extractor
 from yafowil.common import textarea_renderer
+from yafowil.utils import attr_value
+from yafowil.utils import cssid
+from yafowil.utils import generic_html5_attrs
 
 
 def richtext_display_renderer(widget, data):
@@ -12,10 +15,31 @@ def richtext_display_renderer(widget, data):
     return data.tag('div', value, **{'class': 'display-richtext'})
 
 
+def richtext_edit_renderer(widget, data):
+    rendered = textarea_renderer(widget, data)
+    optiontags = []
+    mimetypes = attr_value('mimetypes', widget, data)
+    if not 'text/html' in mimetypes:
+        raise Exception('"text/html" not contained in mimetypes')
+    if len(mimetypes) == 1:
+        return rendered
+    for mimetype in mimetypes:
+        optiontags.append(data.tag('option', mimetype))
+    select_attrs = {
+        'name_': '{}.mimetype'.format(widget.dottedpath),
+        'id': cssid(widget, 'input.mimetype'),
+        'class': attr_value('mimetypes_class', widget, data)
+    }
+    mimetypes_data = attr_value('mimetypes_data', widget, data)
+    select_attrs.update(generic_html5_attrs(mimetypes_data))
+    rendered += data.tag('select', *optiontags, **select_attrs)
+    return rendered
+
+
 factory.register(
     'richtext',
     extractors=[generic_extractor, generic_required_extractor],
-    edit_renderers=[textarea_renderer],
+    edit_renderers=[richtext_edit_renderer],
     display_renderers=[richtext_display_renderer])
 
 factory.doc['blueprint']['richtext'] = \
@@ -43,6 +67,21 @@ factory.doc['props']['richtext.rows'] = \
 factory.defaults['richtext.readonly'] = None
 factory.doc['props']['richtext.readonly'] = \
 """Flag for readonly.
+"""
+
+factory.defaults['richtext.mimetypes'] = ['text/html']
+factory.doc['props']['richtext.mimetypes'] = \
+"""List of mimetypes.
+"""
+
+factory.defaults['richtext.mimetypes_class'] = ''
+factory.doc['props']['richtext.mimetypes_class'] = \
+"""CSS class to be set on mimetypes selection.
+"""
+
+factory.defaults['richtext.mimetypes_data'] = {}
+factory.doc['props']['richtext.mimetypes_data'] = \
+"""Dict containing data attributes to render on mimetypes selection.
 """
 
 factory.defaults['richtext.class'] = 'richtext'
