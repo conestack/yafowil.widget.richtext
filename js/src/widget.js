ee8d5b3c815e496110fb337ce8db3b436092f9a0
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-08-16 12:03:02 +0200

    setup rollup and karma

---
+++ js/src/widget.js
@@ -0,0 +1,48 @@
+/* 
+ * yafowil richtext widget
+ * 
+ * Requires: tinymce
+ * Optional: bdajax
+ */
+
+if (typeof(window['yafowil']) == "undefined") yafowil = {};
+
+(function($) {
+
+    $(document).ready(function() {
+        // initial binding
+        yafowil.richtext.binder();
+
+        // add after ajax binding if bdajax present
+        if (typeof(window['bdajax']) != "undefined") {
+            $.extend(bdajax.binders, {
+                richtext_binder: yafowil.richtext.binder
+            });
+        }
+    });
+
+    $.extend(yafowil, {
+
+        richtext: {
+
+            // tinymce options. extend or override as desired
+            options: {
+                theme: "advanced",
+                plugins: "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+                theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,|,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo",
+                theme_advanced_buttons2: "tablecontrols",
+                theme_advanced_buttons3: "",
+                theme_advanced_toolbar_location: "top",
+                theme_advanced_toolbar_align: "left",
+                theme_advanced_statusbar_location: "bottom",
+                theme_advanced_resizing: true
+            },
+
+            binder: function(context) {
+                $('textarea.richtext', context)
+                    .tinymce(yafowil.richtext.options);
+            }
+        }
+    });
+
+})(jQuery);
---
+++ src/yafowil/widget/richtext/yafowil.widget.richtext.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+
+
+
+	if (window.yafowil === undefined) {
+	    window.yafowil = {};
+	}
+
+	window.yafowil.richtext = exports;
+
+
+}());
+//# sourceMappingURL=yafowil.widget.richtext.js.map
