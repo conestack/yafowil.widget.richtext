e73893c32e98cbb2aaf5e1b3d98162da888f63b4
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2024-10-21 09:36:23 +0200

    add deprecation message

--- src/yafowil/widget/richtext/example.py
+++ src/yafowil/widget/richtext/example.py
@@ -15,14 +15,40 @@ Richtext widget using TinyMCE.
 """
 
 
+DOC_RICHTEXT_DEPRECATION = """
+.. raw:: html
+
+    <div class="alert alert-info">
+        <i class="bi bi-info-circle-fill"></i>
+        This widget has a newer version available:
+        <a class="link-offset-3"
+           href="../++widget++yafowil.widget.tiptap/index.html">
+            yafowil.widget.tiptap
+        </a>
+    </div>
+    <div class="alert alert-warning">
+        <i class="bi bi-exclamation-triangle-fill"></i>
+        <strong>Deprecation Notice:</strong>
+        yafowil.widget.richtext is 
+        <strong>
+            deprecated
+        </strong>
+        and will no longer receive support or further development.
+    </div>
+"""
+
+
 def richtext():
     part = factory(u'fieldset', name='yafowilwidgetrichtext')
     part['richtext'] = factory('#field:richtext', props={
         'label': 'Richtext field',
         'required': 'Text is required'})
-    return {'widget': part,
-            'doc': DOC_RICHTEXT,
-            'title': 'Richtext'}
+    return {
+        'widget': part,
+        'doc': DOC_RICHTEXT if factory.theme != 'bootstrap5'
+               else DOC_RICHTEXT_DEPRECATION + DOC_RICHTEXT,
+        'title': 'Richtext'
+    }
 
 
 def get_example():
